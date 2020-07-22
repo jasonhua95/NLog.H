0841cec4f43d6a6b84a27d4740f50b75d47de61d
华营杰 <jasonhua12@163.com>
2020-07-22 17:16:30 +0800

    项目初始化

---
+++ NLog.H/Log.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NLog
+{
+    public class Log
+    {
+        static Logger logger = LogManager.GetLogger("");
+        public static void Info(string input)
+        {
+            logger.Info(input);
+        }
+
+        public static void Error(string input)
+        {
+            logger.Error(input);
+        }
+
+        public static void Error(string input, Exception exception)
+        {
+            logger.Error(exception, input);
+        }
+    }
+}
---
+++ NLog.HTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NLog.HTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Info($"Info 1234");
+            Log.Error($"Error 7895");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
